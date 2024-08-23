92a8121eb14faac20fffc9b6704ddf481fbb6acb
paulogbm <duffman919870@gmail.com>
2024-08-23 10:32:18 -0300

    commit

---
+++ variaveis.js
@@ -0,0 +1,39 @@
+const i = 1;
+if(i == 1){
+    const n = 3;
+}
+//console.log(n);
+//i = 2;
+
+var j = 3;
+j = 2;
+var j = 1;
+
+if(true){
+    var x = 7;
+}
+//console.log(x);
+
+let z = 3;
+z = 2;
+
+minhaStringA = 'texto 1';
+minhaStringB = "texto '2'";
+minhaStringC = `texto ${z}`;
+
+//console.log(minhaStringC);
+
+let n1 = .5;
+let n2 = 3;
+let n3 = 1.0;
+
+let b1 = true;
+let b2 = false;
+
+let c1;
+console.log(c1);
+
+let v1 = 2;
+let v2 = 3;
+
+console.log(`O resultado da soma entre ${v1} e ${v2} é ${v1 + v2}`);
