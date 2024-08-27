3083b6b217da031556780ed8b8b74e5fe330a602
paulogbm <duffman919870@gmail.com>
2024-08-27 17:16:23 -0300

    add

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let a = 2;
+let b = 3;
+let c;
+
+c = a + b;
+console.log("O resultado da soma entre " + a + " e " + b + " é " + c);
+
+c = a - b;
+console.log(`O resultado da subtração entre ${a} e ${b} é ${c}`);
+
+c = a * b;
+console.log(`O resultado da multiplicação entre ${a} e ${b} é ${c}`);
+
+c = a / b;
+console.log(`O resultado da divisão entre ${a} e ${b} é ${c}`);
